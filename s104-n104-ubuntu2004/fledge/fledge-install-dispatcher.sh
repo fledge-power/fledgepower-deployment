f75e6ce52b9c16905c28b302141f49db2b096e12
aklira <akli.rahmoun@gmail.com>
2022-09-19 11:45:25 -0400

    fixed problem with dispatcher pkg name
    Signed-off-by: aklira <akli.rahmoun@gmail.com>

--- s104-n104-ubuntu2004/fledge/fledge-install-dispatcher.sh
+++ s104-n104-ubuntu2004/fledge/fledge-install-dispatcher.sh
@@ -21,6 +21,6 @@
 ##
 
 wget --no-check-certificate http://archives.fledge-iot.org/2.0.0/ubuntu2004/x86_64/fledge-service-dispatcher_2.0.0_x86_64.deb
-dpkg --unpack ./fledge-service-dispatcher_1.9.2-51_x86_64.deb
+dpkg --unpack ./fledge-service-dispatcher_2.0.0_x86_64.deb
 apt-get install -yf
 apt-get clean -y
