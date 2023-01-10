d9c78a3c9c02b467b2532282d07a68b875f52512
Yannick Marchetaux <yannick.marchetaux@equans.com>
2023-01-10 16:17:47 +0100

    Change of variable names from French to English
    Signed-off-by: Yannick Marchetaux <yannick.marchetaux@equans.com>

--- s104-n104-ubuntu2004/fledge/importModules.sh
+++ s104-n104-ubuntu2004/fledge/importModules.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Constants
-nom_service_south_s1="iec104south_s1"
-nom_service_south_s2="iec104south_s2"
-nom_service_south_s1_advanced="${nom_service_south_s1}Advanced"
-nom_service_south_s2_advanced="${nom_service_south_s2}Advanced"
+s1_south_service_name="iec104south_s1"
+s2_south_service_name="iec104south_s2"
+s1_south_service_name_advanced="${s1_south_service_name}Advanced"
+s2_south_service_name_advanced="${s2_south_service_name}Advanced"
 
-nom_service_north_n1="iec104north_c1"
-nom_service_north_n2="iec104north_c2"
+n1_north_service_name="iec104north_c1"
+n2_north_service_name="iec104north_c2"
 
 plugin_1="iec104_pivot_filter"
 plugin_2="status-points-timestamping"
@@ -16,12 +16,12 @@ plugin_4="mvscale"
 plugin_5="mvcyclingcheck"
 
 # Create service south
-curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$nom_service_south_s1'","type":"south","plugin":"iec104","enabled":false}'
-curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$nom_service_south_s2'","type":"south","plugin":"iec104","enabled":false}'
+curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$s1_south_service_name'","type":"south","plugin":"iec104","enabled":false}'
+curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$s2_south_service_name'","type":"south","plugin":"iec104","enabled":false}'
 
 # Create service north
-curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$nom_service_north_n1'","type":"north","plugin":"iec104","enabled":false}'
-curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$nom_service_north_n2'","type":"north","plugin":"iec104","enabled":false}'
+curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$n1_north_service_name'","type":"north","plugin":"iec104","enabled":false}'
+curl -sX POST http://localhost:8081/fledge/service -d '{"name":"'$n2_north_service_name'","type":"north","plugin":"iec104","enabled":false}'
 
 # Create plugins
 curl -X POST http://localhost:8081/fledge/filter -d '{"name": "'$plugin_1'", "plugin": "'$plugin_1'"}'
@@ -31,14 +31,14 @@ curl -X POST http://localhost:8081/fledge/filter -d '{"name": "'$plugin_4'", "pl
 curl -X POST http://localhost:8081/fledge/filter -d '{"name": "'$plugin_5'", "plugin": "'$plugin_5'"}'
 
 # Create of south pipelines
-curl -X PUT http://localhost:8081/fledge/filter/$nom_service_south_s1/pipeline -d  '{"pipeline": ["'$plugin_1'", "'$plugin_2'", "'$plugin_3'", "'$plugin_4'", "'$plugin_5'"]}'
-curl -X PUT http://localhost:8081/fledge/filter/$nom_service_south_s2/pipeline -d  '{"pipeline": ["'$plugin_1'", "'$plugin_2'", "'$plugin_3'", "'$plugin_4'", "'$plugin_5'"]}'
+curl -X PUT http://localhost:8081/fledge/filter/$s1_south_service_name/pipeline -d  '{"pipeline": ["'$plugin_1'", "'$plugin_2'", "'$plugin_3'", "'$plugin_4'", "'$plugin_5'"]}'
+curl -X PUT http://localhost:8081/fledge/filter/$s2_south_service_name/pipeline -d  '{"pipeline": ["'$plugin_1'", "'$plugin_2'", "'$plugin_3'", "'$plugin_4'", "'$plugin_5'"]}'
 
 # Create of north pipelines
-curl -X PUT http://localhost:8081/fledge/filter/$nom_service_north_n1/pipeline -d  '{"pipeline": ["'$plugin_1'"]}'
-curl -X PUT http://localhost:8081/fledge/filter/$nom_service_north_n2/pipeline -d  '{"pipeline": ["'$plugin_1'"]}'
+curl -X PUT http://localhost:8081/fledge/filter/$n1_north_service_name/pipeline -d  '{"pipeline": ["'$plugin_1'"]}'
+curl -X PUT http://localhost:8081/fledge/filter/$n2_north_service_name/pipeline -d  '{"pipeline": ["'$plugin_1'"]}'
 
 # Param advanced south
 sleep 5
-curl -X PUT --data '{"maxSendLatency":"100"}' http://localhost:8081/fledge/category/$nom_service_south_s1_advanced
-curl -X PUT --data '{"maxSendLatency":"100"}' http://localhost:8081/fledge/category/$nom_service_south_s2_advanced
+curl -X PUT --data '{"maxSendLatency":"100"}' http://localhost:8081/fledge/category/$s1_south_service_name_advanced
+curl -X PUT --data '{"maxSendLatency":"100"}' http://localhost:8081/fledge/category/$s2_south_service_name_advanced
