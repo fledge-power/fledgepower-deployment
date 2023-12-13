16560c51399f113bf6958835fd5a06794fcb6ea7
AkliRa <39710290+aklira@users.noreply.github.com>
2023-12-13 15:42:14 +0100

    Update fledge-install-dispatcher.sh
    Signed-off-by: AkliRa <39710290+aklira@users.noreply.github.com>

--- s104-nopcua-ubuntu2004/fledge/fledge-install-dispatcher.sh
+++ s104-nopcua-ubuntu2004/fledge/fledge-install-dispatcher.sh
@@ -20,7 +20,7 @@
 ## Author: Mark Riddoch, Akli Rahmoun
 ##
 
-wget --no-check-certificate http://archives.fledge-iot.org/nightly/ubuntu2004/x86_64/fledge-service-dispatcher_2.2.0-10_x86_64.deb
-dpkg --unpack ./fledge-service-dispatcher_2.2.0-10_x86_64.deb
+wget --no-check-certificate http://archives.fledge-iot.org/nightly/ubuntu2004/x86_64/fledge-service-dispatcher_2.2.0-15_x86_64.deb
+dpkg --unpack ./fledge-service-dispatcher_2.2.0-15_x86_64.deb
 apt-get install -yf
 apt-get clean -y
