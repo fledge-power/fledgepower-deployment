8d499ede3596a902a61e7fcb18e41de4b32a67c9
AkliRa <39710290+aklira@users.noreply.github.com>
2024-06-14 16:50:19 +0200

    Update fledge-south-tase2.sh
    Signed-off-by: AkliRa <39710290+aklira@users.noreply.github.com>

--- all-plugins-ubuntu2004/fledge/fledge-south-tase2.sh
+++ all-plugins-ubuntu2004/fledge/fledge-south-tase2.sh
@@ -29,7 +29,7 @@ cd fledge-south-tase2
 chmod +x mkversion
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DFLEDGE_INCLUDE=/usr/local/fledge/include/ -DFLEDGE_LIB=/usr/local/fledge/lib/ ..
+cmake -DCMAKE_BUILD_TYPE=Release -DFLEDGE_INCLUDE=/usr/local/fledge/include/ -DFLEDGE_LIB=/usr/local/fledge/lib/ -DCMAKE_PREFIX_PATH=/usr/local/fledge ..
 make
 if [ ! -d "${FLEDGE_ROOT}/plugins/south/tase2" ] 
 then
